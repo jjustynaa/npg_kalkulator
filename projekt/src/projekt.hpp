60f415c6aa5b1cccef28468adbd37e13bf0ad7b8
L-JANUSZ <163130624+L-JANUSZ@users.noreply.github.com>
2024-05-05 13:12:04 +0200

    Update projekt.hpp
    Updated projekt.hpp
    addition, substraction, division, multiplication, exponential and modulo

---
+++ projekt/src/projekt.hpp
@@ -0,0 +1,25 @@
+#ifndef PROJEKT_PROJEKT_HPP
+#define PROJEKT_PROJEKT_HPP
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+
+void cykl_o_tryg();
+
+void cykl();
+
+void tryg();
+
+void print_zao();
+double addition(double num1, double num2);
+double subtraction(double num1, double num2);
+double division(double num1, double num2);
+double multiplication(double num1, double num2);
+double exponential(double num1, double num2);
+int modulo(int num1, int num2);
+
+
+#endif //PROJEKT_PROJEKT_HPP
+
