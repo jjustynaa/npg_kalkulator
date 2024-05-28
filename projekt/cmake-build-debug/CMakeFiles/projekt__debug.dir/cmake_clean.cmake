3f27aea59fa0d9a473575467c31fe70c47204268
Natalia <guzdekn@student.agh.edu.pl>
2024-05-28 18:50:59 +0200

    "Poprawa menu kalkulatora"

---
+++ projekt/cmake-build-debug/CMakeFiles/projekt__debug.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt__debug.dir/main.cpp.obj"
+  "CMakeFiles/projekt__debug.dir/main.cpp.obj.d"
+  "CMakeFiles/projekt__debug.dir/src/projekt.cpp.obj"
+  "CMakeFiles/projekt__debug.dir/src/projekt.cpp.obj.d"
+  "libprojekt__debug.dll.a"
+  "projekt__debug.exe"
+  "projekt__debug.exe.manifest"
+  "projekt__debug.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projekt__debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ projekt/cmake-build-debug/CMakeFiles/projekt__test.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for projekt__test.
+# This may be replaced when dependencies are built.
