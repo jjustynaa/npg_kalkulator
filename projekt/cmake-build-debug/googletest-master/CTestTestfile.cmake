d5fc5f0016b2b72d995ab286cadb966d35b5c912
M-GENEROWICZ <163130340+M-GENEROWICZ@users.noreply.github.com>
2024-05-29 22:29:26 +0200

    Delete projekt/cmake-build-debug directory

--- projekt/cmake-build-debug/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ASUS/Desktop/kalkulator/googletest-master
-# Build directory: C:/Users/ASUS/Desktop/kalkulator/projekt/cmake-build-debug/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- projekt/cmake-build-debug/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ASUS/Desktop/kalkulator/googletest-master/googletest
-# Build directory: C:/Users/ASUS/Desktop/kalkulator/projekt/cmake-build-debug/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
