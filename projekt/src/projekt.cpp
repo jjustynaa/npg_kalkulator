040cf42a9f04670a18e2fb5d77335bd1a2627e3d
M-GENEROWICZ <163130340+M-GENEROWICZ@users.noreply.github.com>
2024-05-29 18:39:04 +0200

    Dodawanie funkcji logarytmów

--- projekt/src/projekt.cpp
+++ projekt/src/projekt.cpp
@@ -98,8 +98,10 @@ std::vector<std::vector<int>> createMatrix(std::size_t rows, std::size_t col) {
     std::vector<std::vector<int>> matrix(rows, std::vector<int>(col));
 
     for(std::size_t i = 0; i < rows; i++)
-        for(std::size_t j = 0; j < col; j++)
+        for(std::size_t j = 0; j < col; j++) {
+            std::cout << "[ " << i+1 << ", " << j+1 << "]:";
             std::cin >> matrix[i][j];
+        }
 
     return matrix;
 }
@@ -413,6 +415,11 @@ double Complex::clasic_to_tryg() const {
     return 7312;
 }
 
+Complex inerface_complex(Complex& cpx){
+    Complex cpx_int(cpx.Re, -cpx.Im);
+    return cpx_int;
+}
+
 void print_complex(Complex cpx){
     if (cpx.Phi != 7321) {
         std::cout << std::fixed << std::setprecision(3) << cpx.Re << " + i" << cpx.Im << "\ncos(" << cpx.Phi << ") + isin(" << cpx.Phi << ")" << std::endl;
@@ -492,6 +499,19 @@ bool does_triangle_exist(double a, double b, double c){
     return false;
 }
 
+double triangleArea1(double a, double h){
+    return a * h / 2;
+}
+
+double triangleArea2(double a, double b, double c){
+    double p = (a + b + c) / 2;
+    return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+double triangleArea3(double a, double b, double angle){
+    return a * b * sin(angle) / 2;
+}
+
 void triangle(){
     triangle:
     std::cout << "Wybierz jakim sposobem chcialbys obliczyc pole trojkata\n1 - dlugosc boku i wysokosci na niej opuszczonej\n"
@@ -500,7 +520,7 @@ void triangle(){
     std::cout << "Wybierz cyfre:\n";
     std::cin >> mark;
 
-    double a, b, c, h, p, angle;
+    double a, b, c, h, angle;
     double area;
 
     switch( mark ){
@@ -520,7 +540,7 @@ void triangle(){
                 std::cout << "Podano nieprawidlowa dlugosc. Sprobuj ponownie." << std::endl;
                 goto wysokosc11;
             }
-            area = a * h / 2;
+            area = triangleArea1(a, h);
             break;
 
         case 2:
@@ -549,8 +569,7 @@ void triangle(){
             }
 
             if(does_triangle_exist(a,b,c) && does_triangle_exist(c,a,b) && does_triangle_exist(b,c,a)) {
-                p = (a + b + c) / 2;
-                area = sqrt(p * (p - a) * (p - b) * (p - c));
+                area = triangleArea2(a, b, c);
             }
             else {
                 std::cout << "Podano nieprawidlowe dlugosci w trojkacie. Trojkat nie istnieje.\n"
@@ -583,7 +602,7 @@ void triangle(){
                 goto miara;
             }
 
-            area = a * b * sin(angle) / 2;
+            area = triangleArea3(a, b, angle);
             break;
         default:
             std::cout << "Wybrano nieprawidlowa operacje" << std::endl;
@@ -592,6 +611,10 @@ void triangle(){
     }
 
     std::cout << "Pole trojkata wynosi: " << area << std::endl;
+}
+
+double rectangularArea(double a, double b){
+    return a * b;
 }
 
 void rectangular(){
@@ -613,13 +636,115 @@ void rectangular(){
         std::cout << "Podano nieprawidlowa dlugosc. Sprobuj ponownie." << std::endl;
         goto bok2;
     }
-    area = a * b;
+    area = rectangularArea(a, b);
     std::cout << "Pole prostokatu wynosi: " << area << std::endl;
+}
+
+double diamondArea1(double a, double h){
+    return a * h;
+}
+
+double diamondArea2(double e, double f){
+    return e * f / 2;
+}
+
+void diamond(){
+    diamond:
+    std::cout << "Wybierz jakim sposobem chcialbys obliczyc pole trapezu\n1 - dlugosc boku i wysokosci na niej opuszczonej\n"
+                 "2 - znane sa dlugosci przekatnych rombu" << std::endl;
+
+    int mark;
+    std::cout << "Wybierz cyfre:\n";
+    std::cin >> mark;
+
+    double a, h;
+    double area;
+
+    switch( mark ){
+        case 1:
+            std::cout << "Obliczanie pola przez podanie boku i wysokosci." << std::endl;
+        bok11:
+            std::cout << "Dlugosc boku:\n";
+            std::cin >> a;
+            if (a <= 0 ){
+                std::cout << "Podano nieprawidlowa dlugosc. Sprobuj ponownie." << std::endl;
+                goto bok11;
+            }
+        wysokosc11:
+            std::cout << "Dlugosc wysokosci:\n";
+            std::cin >> h;
+            if (h <= 0 ){
+                std::cout << "Podano nieprawidlowa dlugosc. Sprobuj ponownie." << std::endl;
+                goto wysokosc11;
+            }
+            area = diamondArea1(a, h);
+            break;
+
+        case 2:
+            std::cout << "Obliczanie pola przez podanie dlugosci przekatnych." << std::endl;
+        bok21:
+            std::cout << "Dlugosc pierwszej przekatnej:\n";
+            std::cin >> a;
+            if (a <= 0 ){
+                std::cout << "Podano nieprawidlowa dlugosc. Sprobuj ponownie." << std::endl;
+                goto bok21;
+            }
+        bok22:
+            std::cout << "Dlugosc drugiej przekatnej:\n";
+            std::cin >> h;
+            if (h <= 0 ){
+                std::cout << "Podano nieprawidlowa dlugosc. Sprobuj ponownie." << std::endl;
+                goto bok22;
+            }
+            area = diamondArea2(a, h);
+            break;
+
+        default:
+            std::cout << "Wybrano nieprawidlowa operacje" << std::endl;
+            //blad();
+            goto diamond;
+    }
+    std::cout << "Pole rombu wynosi: " << area << std::endl;
+}
+
+double trapezeArea(double a, double b, double h){
+    return (a + b) * h / 2;
+}
+
+void trapeze(){
+    double a;
+    double b;
+    double h;
+    double area;
+    std::cout << "Podaj dlugosci podstaw trapezu: " << std::endl;
+    bok1:
+    std::cout << "Dlugosc pierwszej podstawy:\n";
+    std::cin >> a;
+    if (a <= 0 ){
+        std::cout << "Podano nieprawidlowa dlugosc. Sprobuj ponownie." << std::endl;
+        goto bok1;
+    }
+    bok2:
+    std::cout << "Dlugosc drugiej podstawy:\n";
+    std::cin >> b;
+    if (b <= 0 ){
+        std::cout << "Podano nieprawidlowa dlugosc. Sprobuj ponownie." << std::endl;
+        goto bok2;
+    }
+    wysokosc:
+    std::cout << "Dlugosc wysokosci:\n";
+    std::cin >> h;
+    if (h <= 0 ){
+        std::cout << "Podano nieprawidlowa dlugosc. Sprobuj ponownie." << std::endl;
+        goto wysokosc;
+    }
+    area = trapezeArea(a, b, h);
+    std::cout << "Pole trapezu wynosi: " << area << std::endl;
 }
 
 void area(){
     area:
-    std::cout <<"Wybierz dzialanie programu:\nP obliczanie pola prostokata\nT obliczanie pola trojkata" << std::endl;
+    std::cout <<"Wybierz dzialanie programu:\nP obliczanie pola prostokata\nT obliczanie pola trojkata\nR obliczanie pola rombu\nN obliczanie pola trapezu" << std::endl;
 
     char mark;
     std::cin >> mark;
@@ -630,9 +755,23 @@ void area(){
         case 't':
             triangle();
             break;
+        case 'r':
+            diamond();
+            break;
+        case 'n':
+            trapeze();
+            break;
         default:
             std::cout << "Wybrano nieprawidlowa operacje" << std::endl;
             //blad();
             goto area;
     }
+}
+
+float logarytmy(){
+    std::cout << "Podaj liczbe do zlogarytmowanie i podstawe logarytmu:" << std::endl;
+    float ll, base;
+    std::cin >> ll >> base;
+    float wynik = log(ll) / log(base);
+    return wynik;
 }
