d789265eb6c88383979d7a22e11ecc7d1ea43e45
Justyna J <j.justyna214@gmail.com>
2024-06-01 17:34:52 +0200

    PW - część I i II

---
+++ projekt/include/value_conventer.hpp
@@ -0,0 +1,9 @@
+#ifndef PROJEKT_VALUE_CONVENTER_HPP
+#define PROJEKT_VALUE_CONVENTER_HPP
+
+double conventer_speed();
+double conventer_length();
+double conventer_time();
+void value_conventer();
+
+#endif //PROJEKT_VALUE_CONVENTER_HPP
